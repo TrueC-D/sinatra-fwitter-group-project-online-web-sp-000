e3c4fa9f4f562400863c5abc11cfa065506ffd72
Alethea Cary <aletheacary@gmail.com>
2019-10-17 09:33:23 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,5 +6,15 @@ class ApplicationController < Sinatra::Base
     set :public_folder, 'public'
     set :views, 'app/views'
   end
+  
+  get '/' do 
+    "Welcome to Fwitter"
+  end
+  
+  get '/signup' do
+  end
+  
+  get '/login' do
+  end
 
 end
