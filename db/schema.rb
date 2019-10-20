c522c99be211ee41b09816140d70f82eb6163db8
Alethea Cary <aletheacary@gmail.com>
2019-10-20 05:26:41 +0000

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20191020051915) do
+
+  create_table "items", force: :cascade do |t|
+    t.string "content"
+  end
+
+  create_table "user_tweets", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "tweet_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.string "password"
+  end
 
 end
