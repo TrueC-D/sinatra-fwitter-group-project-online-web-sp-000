1994832ebf0807ffaa14fac1693979e57e96d2a1
Alethea Cary <aletheacary@gmail.com>
2019-10-24 05:49:12 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,47 +1,47 @@
 require './config/environment'
+require 'pry'
 
 class ApplicationController < Sinatra::Base
 
   configure do
+    enable :sessions
+    set :session_secret, "S3C43T"
     set :public_folder, 'public'
     set :views, 'app/views'
   end
-  
-  get '/' do 
+
+  get '/' do
     erb :'index'
   end
-  
+
   get '/signup' do
-    # if 
-    # else
       erb :'users/create_user'
-    # end
   end
-  
+
   post '/signup' do
     if params.any? {|key, value| value.strip.length == 0} == true
       redirect '/signup'
     else
-      redirect '/tweets'
       user = User.new(:username => params[:username], :email => params[:email], :pawword => params[:password])
       user.save
+      redirect '/tweets'
     end
   end
-  
+
   get '/login' do
-    
+
     erb :'users/login'
   end
-  
-  post '/login' do 
+
+  post '/login' do
     user = User.find_by(:username => params[:username])
     redirect :'/tweets'
   end
-  
-  get '/tweets' do 
+
+  get '/tweets' do
     erb :'tweets/tweets'
   end
-  
+
   get '/logout' do
   end
 
