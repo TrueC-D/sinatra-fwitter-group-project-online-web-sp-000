e8a54f1b07a885c5e497841f3ae1f951ff1cf21c
Alethea Cary <aletheacary@gmail.com>
2019-10-25 00:30:21 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,8 +15,7 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/signup' do
-    if session[:user_id]
-
+    if User.find_by_id(session[:user_id])
       redirect '/tweets'
     else
       erb :'users/create_user'
