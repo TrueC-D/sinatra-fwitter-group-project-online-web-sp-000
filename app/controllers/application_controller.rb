1d732dd67cbfdcce347c050d57fedf216e22893c
Alethea Cary <aletheacary@gmail.com>
2019-10-25 01:03:36 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,45 +14,9 @@ class ApplicationController < Sinatra::Base
     erb :'index'
   end
 
-  get '/signup' do
-    if User.find_by_id(session[:user_id])
-      redirect '/tweets'
-    else
-      erb :'users/create_user'
-    end
-  end
-
-  post '/signup' do
-    if params.any? {|key, value| value.strip.length == 0} == true
-      redirect '/signup'
-    else
-      user = User.new(:username => params[:username], :email => params[:email], :password => params[:password])
-      user.save
-      redirect '/tweets'
-    end
-  end
-
-  get '/login' do
-    erb :'users/login'
-  end
-
-  post '/login' do
-    @user = User.find_by(:username => params[:username])
-    if @user != nil && @user.password == params[:password]
-      session[:user_id] == @user.id
-      redirect :'/tweets'
-    else
-      redirect :'/login'
-    end
-  end
 
   get '/tweets' do
     erb :'tweets/tweets'
-  end
-
-  get '/logout' do
-    session.clear
-    redirect '/'
   end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,40 @@
 class UsersController < ApplicationController
+  get '/signup' do
+    @current_user = User.find_by_id(session[:user_id])
+    binding.pry
+    if @current_user
+      redirect '/tweets'
+    else
+      erb :'users/create_user'
+    end
+  end
 
+  post '/signup' do
+    if params.any? {|key, value| value.strip.length == 0} == true
+      redirect '/signup'
+    else
+      user = User.new(:username => params[:username], :email => params[:email], :password => params[:password])
+      user.save
+      redirect '/tweets'
+    end
+  end
 
+  get '/login' do
+    erb :'users/login'
+  end
+
+  post '/login' do
+    @user = User.find_by(:username => params[:username])
+    if @user != nil && @user.password == params[:password]
+      session[:user_id] == @user.id
+      redirect :'/tweets'
+    else
+      redirect :'/login'
+    end
+  end
+
+  get '/logout' do
+    session.clear
+    redirect '/'
+  end
 end
