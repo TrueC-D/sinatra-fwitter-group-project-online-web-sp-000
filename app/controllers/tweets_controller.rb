2b8ea857131ebae26e50b83ba557da68b84942f3
Alethea Cary <aletheacary@gmail.com>
2019-10-31 06:54:24 +0000

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -13,6 +13,16 @@ class TweetsController < ApplicationController
     erb :'tweets/new'
   end
 
+  post '/tweets' do
+    if current_user && params[:content].strip.length != 0
+      tweet = Tweet.new(:content => params[:content], :user_id => current_user.id)
+      tweet.save
+      redirect '/tweets'
+    else
+      redirect '/tweets/new'
+    end
+  end
+
   get '/tweets/#{tweet.id}'  do
     erb :'tweets/show_tweet'
   end
